03c4826e14743c65401022d1aef8a0eeb5dfbdfd
Mykel Valadez <valadezmykel@gmail.com>
2020-12-12 09:50:22 -0800

    defined presents table in db using model.presents

---
+++ models/presents.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes)=>{
+    const Present = sequelize.define("Present", {
+        present_name: {
+            type: DataTypes.String,
+            allowNull: false,
+            validate: {
+                len: [1,400]
+            }
+        },
+
+    });
+
+    Present.associate = (models) =>{
+        Present.belongsTo(models.user, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+    
+    return Present;
+};
