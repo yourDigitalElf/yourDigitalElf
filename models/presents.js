ef9ca8d8e8badbb90038e39c7808f8cf1f1a4207
Mykel Valadez <valadezmykel@gmail.com>
2020-12-12 14:24:18 -0800

    prettier linter added to package jason and configurations and ingore added

--- models/presents.js
+++ models/presents.js
@@ -1,22 +1,21 @@
-module.exports = (sequelize, DataTypes)=>{
-    const Present = sequelize.define("Present", {
-        present_name: {
-            type: DataTypes.String,
-            allowNull: false,
-            validate: {
-                len: [1,400]
-            }
-        },
+module.exports = (sequelize, DataTypes) => {
+	const Present = sequelize.define("Present", {
+		present_name: {
+			type: DataTypes.String,
+			allowNull: false,
+			validate: {
+				len: [1, 400],
+			},
+		},
+	});
 
-    });
+	Present.associate = models => {
+		Present.belongsTo(models.user, {
+			foreignKey: {
+				allowNull: false,
+			},
+		});
+	};
 
-    Present.associate = (models) =>{
-        Present.belongsTo(models.user, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-    
-    return Present;
+	return Present;
 };
--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+	session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -29,8 +31,12 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-  });
+db.sequelize.sync().then(function () {
+	app.listen(PORT, function () {
+		console.log(
+			"==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+			PORT,
+			PORT
+		);
+	});
 });
