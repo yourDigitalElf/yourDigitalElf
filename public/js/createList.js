ef9d12ffd016f8e50a5692be6f3043670c04f337
Ben Robinson <robinson.dri@gmail.com>
2020-12-20 20:25:35 -0800

    adding delete button

--- public/js/createList.js
+++ public/js/createList.js
@@ -38,5 +38,18 @@ $(document).ready(function () {
             // })
     }
 
+        $(function() {
+            $(".delGift").on("click", function(event) {
+                let id = $(this).data("id");
 
+                $.ajax("/api/presents/" + id, {
+                    type: "DELETE"
+                }).then(
+                    function(){
+                        console.log("deleted id", id);
+                        location.reload();
+                    }
+                )
+            });
+        });
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -106,19 +106,19 @@ module.exports = function (app) {
 	});
 
 	// Route for getting some data about our user to be used client side
-	app.get("/api/user_data", function (req, res) {
-		if (!req.user) {
+	// app.get("/api/user_data", function (req, res) {
+	// 	if (!req.user) {
 			// The user is not logged in, send back an empty object
-			res.json({});
-		} else {
+		// 	res.json({});
+		// } else {
 			// Otherwise send back the user's email and id
 			// Sending back a password, even a hashed password, isn't a good idea
-			res.json({
-				email: req.user.email,
-				id: req.user.id,
-			});
-		}
-	});
+	// 		res.json({
+	// 			email: req.user.email,
+	// 			id: req.user.id,
+	// 		});
+	// 	}
+	// });
 
 
 };
